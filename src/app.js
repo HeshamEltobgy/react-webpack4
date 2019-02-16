770a7ec069c7d6832852d2789644ca7f6b129f31
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-02-16 20:36:38 +0800

    create js and style loaders

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello world ... from Webpack');
+import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
+
+import './app.scss'
+
+class App extends React.Component {
+    render() {
+       return (
+        <p>Hello React!</p>
+      )
+    }
+}
+
+render (
+  <App />,
+  document.getElementById('root')
+
+)
