913299319da8b2fe8218ff9c76d3e7d6ce074dca
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-02-21 11:50:56 +0800

    improve heading style

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.scss'
 class App extends React.Component {
     render() {
        return (
-        <h2>This project uses webpack 4 as a build tool for a full stack React application. It contains the advanced features that increase the performance and decrease load times which can allow the powerful development and deployment of React applications.</h2>
+        <h3>This project uses webpack 4 as a build tool for a full stack React application. It contains the advanced features that increase the performance and decrease load times which can allow the powerful development and deployment of React applications.</h3>
       )
     }
 }
